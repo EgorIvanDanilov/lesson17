b26829cd7f19c9726a4c46d529a7c70dbe24f7ff
Egor Danilov <mellowd94@gmail.com>
2023-10-18 20:11:41 +0200

    add arrays

--- index.ts
+++ index.ts
@@ -12,3 +12,13 @@ type Age = number;
 let myzAge: Age = 30;
 
 let yourAge: number | string = 18;
+
+type Animal = "cat" | "dog";
+let myPet: Animal = "cat";
+
+type ExtendedPet = Animal | "parrot";
+let yourPet: ExtendedPet = "cat";
+//array strings
+let fruits: string[] = ["apple", "orange", "pear"];
+//array boolean
+let answers: boolean[] = [true, false, false];
