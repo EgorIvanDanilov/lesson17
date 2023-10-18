41f12e2dbcf656d8779e96143f0330dbb889b1d8
Egor Danilov <mellowd94@gmail.com>
2023-10-18 19:26:12 +0200

    add new type

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
 console.log("welcome to ts");
+let x: string = "hello";
+console.log(x);
+
+let y: string;
+y = "Peter";
+
+let year: number = 1994;
+
+type Age = number;
+
+let myzAge: Age = 30;
+
+let yourAge: number | string = 18;
