7c2828b11d05cbb631a7c0a27610b5d2d9755afe
Egor Danilov <mellowd94@gmail.com>
2023-10-20 19:13:38 +0200

    add generics

--- hw.ts
+++ hw.ts
@@ -1,32 +1,49 @@
 //1
 function getSecondElement(arr: string[]): string {
-return arr[1];
+  return arr[1];
 }
-const res = getSecondElement(['Earth', 'Mars', 'Saturn'])
+const res = getSecondElement(["Earth", "Mars", "Saturn"]);
 console.log(res);
 //2
 
-const getWeatherDesc = (code:'sun'|'strm'|'fog') => {
-    switch (code) {
-        case 'sun':return 'sunny';
-        case 'strm':return 'storm';
-        case 'fog':return 'foggy weather';
-        default:return '';
-    }
-}
+const getWeatherDesc = (code: "sun" | "strm" | "fog") => {
+  switch (code) {
+    case "sun":
+      return "sunny";
+    case "strm":
+      return "storm";
+    case "fog":
+      return "foggy weather";
+    default:
+      return "";
+  }
+};
 console.log(getWeatherDesc("sun"));
 
 //3
 interface Airplane {
-    numberOfEngines:number;
-    isJet:boolean;
-    maxHeight:number;
-    capacity?:number;
+  numberOfEngines: number;
+  isJet: boolean;
+  maxHeight: number;
+  capacity?: number;
 }
 
-сonst boeing: Airplane = {
-    numberOfEngines:4,
-    isJet:false,
-    maxHeight:200,
-    capacity:300,
+const boeing: Airplane = {
+  numberOfEngines: 4,
+  isJet: false,
+  maxHeight: 200,
+  capacity: 300,
+};
+
+// generics
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
 }
+console.log(getFirstElement<string>(["Earth", "Mars", "Saturn"]));
+console.log(getFirstElement<number>([23, 1, 44]));
+
+function consoleSmth<T>(smth: T): void {
+  console.log(name);
+}
+consoleSmth(112);
+consoleSmth("Peter");
